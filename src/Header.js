20c7543e966d508e1a0e8fea7d98c77ec1b04759
petramarkovic <petramarkovic996@gmail.com>
2021-01-05 15:26:38 +0100

    changes

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ class Header extends Component{
     render(){
         return(
             <header>
-                <h1>TRAINING EVALUATION</h1>
+                <h1>TRAINING EVALUATION!</h1>
                 <img alt="fitness-img" src="https://i.pinimg.com/originals/d5/9c/5e/d59c5e7431c468c1f816eafd0b8b249f.gif"></img>
             </header>
         );
